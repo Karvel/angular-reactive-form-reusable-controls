fbf18661b58d6d7bdeb38fdb6af83dacbe80f5e9
Elanna Grossman <elanna.grossman@gmail.com>
2018-05-14 22:20:43 -0700

    feat(controls): add `RequiredInputComponent`

---
+++ src/app/core/controls/required-input/required-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector    : 'app-required-input',
+	templateUrl : './required-input.component.html',
+	styleUrls   : ['./required-input.component.scss']
+})
+export class RequiredInputComponent {
+	constructor() { }
+}
