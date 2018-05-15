d294a8d5cf5c2c1a322804551221a931071fdd66
Elanna Grossman <elanna.grossman@gmail.com>
2018-05-14 23:30:51 -0700

    feat(controls): wire up `ReactiveFormsModule`

--- src/app/core/controls/controls.module.ts
+++ src/app/core/controls/controls.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }               from '@angular/core';
 import { CommonModule }           from '@angular/common';
+import { ReactiveFormsModule }    from '@angular/forms';
 
 import { RequiredInputComponent } from './required-input/required-input.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
+		ReactiveFormsModule,
 	],
 	declarations: [
 		RequiredInputComponent,
