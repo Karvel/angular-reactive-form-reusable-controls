a318fd85d50ecd1f5536e1bec043a4931352153e
Elanna Grossman <elanna.grossman@gmail.com>
2018-05-18 22:50:05 -0700

    feat(home): add password field to demonstrate local use of validator

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit }      from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { RequiredValidation }     from '../../core/validation/required-validation';
+
 @Component({
 	selector    : 'app-home',
 	templateUrl : './home.component.html',
@@ -14,6 +16,7 @@ export class HomeComponent implements OnInit {
 	) {
 		this.tutorialForm = this.formBuilder.group({
 			name: '',
+			password: ['', [RequiredValidation.required('Password')]],
 		});
 	}
 
