a843051d77019b9f222401e67bcbc52805aaedbb
Elanna Grossman <elanna.grossman@gmail.com>
2018-06-11 21:37:38 -0700

    feat(material): import `MatCardModule`

--- src/app/core/imports/material.module.ts
+++ src/app/core/imports/material.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 
 import {
 	MatButtonModule,
+	MatCardModule,
 	MatInputModule,
 } from '@angular/material';
 
 @NgModule({
 	imports: [
 		MatButtonModule,
+		MatCardModule,
 		MatInputModule,
 	],
 	exports: [
 		MatButtonModule,
+		MatCardModule,
 		MatInputModule,
 	],
 })
