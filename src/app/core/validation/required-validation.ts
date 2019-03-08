33293c0878ccbf8f02f2abe382143ddf6fdfab44
Elanna Grossman <elanna.grossman@gmail.com>
2019-03-07 20:22:53 -0800

    docs(required-validation): add usage documentation to validation method

--- src/app/core/validation/required-validation.ts
+++ src/app/core/validation/required-validation.ts
@@ -1,6 +1,9 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export class RequiredValidation {
+/**
+ * Returns an error if the passed in control has no value.
+ */
 	static required(placeholder: string = 'This field'): ValidatorFn {
 		return (control: AbstractControl): ValidationErrors | null => {
 			if (!control.value) {
